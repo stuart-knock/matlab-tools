1ffda9b1b1ed582c6f38bd38c557e2efc6d2df90
stuart-knock <stuart.knock@gmail.com>
2018-04-28 18:33:16 +1000

    Changes MesgID for this repo. Adds TODO. Minor clean-up.

--- set_default_groot.m
+++ set_default_groot.m
@@ -31,35 +31,38 @@
 %}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 function [precall_default_groot] = set_default_groot(theme)
     if nargin < 1 || isempty(theme) || ~(isstruct(theme) || ischar(theme))
-        error(['BLN:' mfilename ':BadArgs'], 'You must specify a theme.')
+        error(['SAK:' mfilename ':BadArgs'], 'You must specify a theme.')
     end
 
     if ischar(theme) && nargout < 1
-        error(['BLN:' mfilename ':NoPreStateAssignment'], ...
-              'You must capture the pre-call state to be able to undo the changes...');
+        error(['SAK:' mfilename ':NoPreStateAssignment'], ...
+              'The pre-call state must be captured to enable undoing changes.');
     end
 
     precall_default_groot = get(groot, 'default');
 
     %% Set the default groot values based on struct with fields.
     if isstruct(theme)
+        theme_default_fields = fieldnames(theme);
+        %TODO(stuart-knock): Add some checks that the provided theme struct
+        %                    is actually the result of a previous call to
+        %                    get(groot, 'default').
+
         % Remove all current default groot values.
-        default_fields = fieldnames(precall_default_groot);
-        for index = 1:length(default_fields)
-            set(groot, default_fields{index}, 'remove');
+        precall_default_fields = fieldnames(precall_default_groot);
+        for index = 1:length(precall_default_fields)
+            set(groot, precall_default_fields{index}, 'remove');
         end
 
         % Set default groot values based on input struct.
-        default_fields = fieldnames(theme);
-        for index = 1:length(default_fields)
-            this_default = default_fields{index};
+        for index = 1:length(theme_default_fields)
+            this_default = theme_default_fields{index};
             set(groot, this_default, theme.(this_default));
         end
         return
-    end
+    end %isstruct(theme)
 
     %% Apply the default groot values for the requested theme.
     switch lower(theme)
@@ -70,7 +73,7 @@ function [precall_default_groot] = set_default_groot(theme)
             set(groot, 'defaultAxesXColor',  [255, 255, 255] ./ 255);
             set(groot, 'defaultFigureColor', [ 25,  25,  25] ./ 255);
             set(groot, 'defaultTextColor',   [255, 255, 255] ./ 255);
-            
+
         case 'paper'
             %% Set a paper colour theme.
             set(groot, 'defaultAxesColor',   [255, 255, 255] ./ 255);
@@ -78,30 +81,25 @@ function [precall_default_groot] = set_default_groot(theme)
             set(groot, 'defaultAxesXColor',  [  0,   0,   0] ./ 255);
             set(groot, 'defaultFigureColor', [255, 255, 255] ./ 255);
             set(groot, 'defaultTextColor',   [  0,   0,   0] ./ 255);
-            set(groot, 'defaultTextFontSize',   20);
-            set(groot, 'defaultAxesGridAlpha',1);
-            set(groot, 'defaultAxesGridColor',[0.1 0.1 0.1]);
-            set(groot, 'defaultLineLineWidth',2);
-            set(groot, 'defaultAxesFontSize',20);
-            set(groot, 'defaultAxesBox','on');
-            set(groot, 'defaultAxesLineWidth',2);
-            set(groot, 'defaultFigurePaperUnits','centimeters');
-            set(groot, 'defaultFigureUnits','centimeters');
-            set(groot, 'defaultFigurePaperSize',[21 29.7]);
-            set(groot, 'defaultFigurePaperOrientation','landscape');
-            set(groot, 'defaultFigurePaperPositionMode','auto');
-            set(groot, 'defaultFigurePaperType','a4');
-            set(groot, 'defaultFigurePosition',[2 2 20 12]);
-
-
-
-
-
+            set(groot, 'defaultTextFontSize', 20);
+            set(groot, 'defaultAxesGridAlpha', 1);
+            set(groot, 'defaultAxesGridColor', [0.1 0.1 0.1]);
+            set(groot, 'defaultLineLineWidth', 2);
+            set(groot, 'defaultAxesFontSize', 20);
+            set(groot, 'defaultAxesBox', 'on');
+            set(groot, 'defaultAxesLineWidth', 2);
+            set(groot, 'defaultFigurePaperUnits', 'centimeters');
+            set(groot, 'defaultFigureUnits', 'centimeters');
+            set(groot, 'defaultFigurePaperSize', [21 29.7]);
+            set(groot, 'defaultFigurePaperOrientation', 'landscape');
+            set(groot, 'defaultFigurePaperPositionMode', 'auto');
+            set(groot, 'defaultFigurePaperType', 'a4');
+            set(groot, 'defaultFigurePosition', [2 2 20 12]);
 
         %TODO: define more themes...
         otherwise
-            error(['BLN:' mfilename ':UnknownTheme'], ...
+            error(['SAK:' mfilename ':UnknownTheme'], ...
                   'The theme you specified is not recognised: "%s".', theme);
-    end
+    end %switch lower(theme)
 
 end % function set_default_groot()
