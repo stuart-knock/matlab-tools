c19ab2ae36653548a63664039a9976fd916badff
stuart-knock <stuart.knock@gmail.com>
2018-06-08 11:57:33 +1000

    Fixes printing of non-white background themes. Adds new themes.
      + Turns off FigureInvertHardcopy so that printing preserves
        coloured backgrounds.
      + Adds a default/matlab theme -- resets all default graphics
        settings to Matlab default values.
      + Adds a grey/gray/winter theme -- dark grey background, light
        grey fonts and lines, default colormap set as 'winter'.
      + Adds a rand theme -- random colours, because, why not...
      + Adds font/line/figure sizing to 'dark' theme.
      + Changes default font size on 'paper' theme to 16, following
        discussions with PSL.

--- set_default_groot.m
+++ set_default_groot.m
@@ -1,7 +1,9 @@
 %% Sets a default graphics theme based on predefined sets of default properties.
 %
 %  When setting a theme, a struct is returned containing a field for each groot
-%  default value as it was prior to the call to this function.
+%  default value as it was prior to the call to this function. This is to enable
+%  resetting to the pre-call state, so side effects can be prevented when used
+%  within a plotting function.
 %
 %  ARGUMENTS:
 %    theme -- a string indicating a predefined theme or a struct specifying a
@@ -36,25 +38,26 @@ function [precall_default_groot] = set_default_groot(theme)
         error(['SAK:' mfilename ':BadArgs'], 'You must specify a theme.')
     end
 
-    if ischar(theme) && nargout < 1
+    if ischar(theme) && ~ismember(theme, {'default', 'matlab'}) && nargout < 1
         error(['SAK:' mfilename ':NoPreStateAssignment'], ...
               'The pre-call state must be captured to enable undoing changes.');
     end
 
+    % Capture the pre-call default groot state.
     precall_default_groot = get(groot, 'default');
+
+    % Remove all current, user set, default groot values.
+    precall_default_fields = fieldnames(precall_default_groot);
+    for index = 1:length(precall_default_fields)
+        set(groot, precall_default_fields{index}, 'remove');
+    end
 
     %% Set the default groot values based on struct with fields.
     if isstruct(theme)
-        theme_default_fields = fieldnames(theme);
         %TODO(stuart-knock): Add some checks that the provided theme struct
         %                    is actually the result of a previous call to
         %                    get(groot, 'default').
-
-        % Remove all current default groot values.
-        precall_default_fields = fieldnames(precall_default_groot);
-        for index = 1:length(precall_default_fields)
-            set(groot, precall_default_fields{index}, 'remove');
-        end
+        theme_default_fields = fieldnames(theme);
 
         % Set default groot values based on input struct.
         for index = 1:length(theme_default_fields)
@@ -66,35 +69,82 @@ function [precall_default_groot] = set_default_groot(theme)
 
     %% Apply the default groot values for the requested theme.
     switch lower(theme)
+        case {'default', 'matlab'}
+            %% Reset to the default Matlab theme.
+            return
+
         case 'dark'
-            %% Set a dark colour theme.
-            set(groot, 'defaultAxesColor',   [ 25,  25,  25] ./ 255);
-            set(groot, 'defaultAxesYColor',  [255, 255, 255] ./ 255);
-            set(groot, 'defaultAxesXColor',  [255, 255, 255] ./ 255);
-            set(groot, 'defaultFigureColor', [ 25,  25,  25] ./ 255);
-            set(groot, 'defaultTextColor',   [255, 255, 255] ./ 255);
+            %% Set a dark theme.
+            set(groot, 'defaultFigureColor',       [ 25,  25,  25] ./ 255);
+            set(groot, 'defaultAxesColor',         [ 25,  25,  25] ./ 255);
+            set(groot, 'defaultAxesYColor',        [255, 255, 255] ./ 255);
+            set(groot, 'defaultAxesXColor',        [255, 255, 255] ./ 255);
+            set(groot, 'defaultTextColor',         [255, 255, 255] ./ 255);
+            set(groot, 'defaultTextFontSize',      16);
+            set(groot, 'defaultAxesFontSize',      16);
+            set(groot, 'defaultAxesLineWidth',      2);
+            set(groot, 'defaultLineLineWidth',      2);
+            set(groot, 'defaultAxesBox',           'on');
+            set(groot, 'defaultFigurePaperUnits',  'centimeters');
+            set(groot, 'defaultFigureUnits',       'centimeters');
+            set(groot, 'defaultFigurePaperSize',   [10 18]);
+            set(groot, 'defaultFigurePosition',    [2 2 20 12]);
+            set(groot, 'defaultFigurePaperPositionMode', 'auto');
+            set(groot, 'defaultFigurePaperOrientation',  'landscape');
+            set(groot, 'defaultFigureInvertHardcopy', 'off');
+
+        case {'grey', 'gray', 'winter'}
+            %% Set a grey theme.
+            set(groot, 'defaultFigureColor',      [ 64,  64,  64] ./ 255);
+            set(groot, 'defaultAxesColor',        [ 64,  64,  64] ./ 255);
+            set(groot, 'defaultAxesYColor',       [128, 128, 128] ./ 255);
+            set(groot, 'defaultAxesXColor',       [128, 128, 128] ./ 255);
+            set(groot, 'defaultTextColor',        [128, 128, 128] ./ 255);
+            set(groot, 'defaultAxesGridColor',    [ 96,  96,  96] ./ 255);
+            set(groot, 'DefaultFigureColormap',   'winter');
+            set(groot, 'defaultTextFontSize',     16);
+            set(groot, 'defaultAxesFontSize',     16);
+            set(groot, 'defaultLineLineWidth',     2);
+            set(groot, 'defaultAxesLineWidth',     2);
+            set(groot, 'defaultAxesBox',          'off');
+            set(groot, 'defaultFigurePaperUnits', 'centimeters');
+            set(groot, 'defaultFigureUnits',      'centimeters');
+            set(groot, 'defaultFigurePaperSize',  [10 18]);
+            set(groot, 'defaultFigurePosition',   [2 2 20 12]);
+            set(groot, 'defaultFigurePaperPositionMode', 'auto');
+            set(groot, 'defaultFigurePaperOrientation',  'landscape');
+            set(groot, 'defaultFigureInvertHardcopy', 'off');
 
         case 'paper'
-            %% Set a paper colour theme.
-            set(groot, 'defaultAxesColor',   [255, 255, 255] ./ 255);
-            set(groot, 'defaultAxesYColor',  [  0,   0,   0] ./ 255);
-            set(groot, 'defaultAxesXColor',  [  0,   0,   0] ./ 255);
-            set(groot, 'defaultFigureColor', [255, 255, 255] ./ 255);
-            set(groot, 'defaultTextColor',   [  0,   0,   0] ./ 255);
-            set(groot, 'defaultTextFontSize', 20);
-            set(groot, 'defaultAxesGridAlpha', 1);
-            set(groot, 'defaultAxesGridColor', [0.1 0.1 0.1]);
-            set(groot, 'defaultLineLineWidth', 2);
-            set(groot, 'defaultAxesFontSize', 20);
-            set(groot, 'defaultAxesBox', 'on');
-            set(groot, 'defaultAxesLineWidth', 2);
+            %% Set a paper theme.
+            set(groot, 'defaultAxesColor',        [255, 255, 255] ./ 255);
+            set(groot, 'defaultAxesYColor',       [  0,   0,   0] ./ 255);
+            set(groot, 'defaultAxesXColor',       [  0,   0,   0] ./ 255);
+            set(groot, 'defaultFigureColor',      [255, 255, 255] ./ 255);
+            set(groot, 'defaultTextColor',        [  0,   0,   0] ./ 255);
+            set(groot, 'defaultAxesGridColor',    [ 26,  26,  26] ./ 255);
+            set(groot, 'defaultAxesGridAlpha',     1);
+            set(groot, 'defaultTextFontSize',     16);
+            set(groot, 'defaultAxesFontSize',     16);
+            set(groot, 'defaultLineLineWidth',     2);
+            set(groot, 'defaultAxesLineWidth',     2);
+            set(groot, 'defaultAxesBox',          'on');
             set(groot, 'defaultFigurePaperUnits', 'centimeters');
-            set(groot, 'defaultFigureUnits', 'centimeters');
+            set(groot, 'defaultFigureUnits',      'centimeters');
             set(groot, 'defaultFigurePaperSize', [21 29.7]);
-            set(groot, 'defaultFigurePaperOrientation', 'landscape');
-            set(groot, 'defaultFigurePaperPositionMode', 'auto');
             set(groot, 'defaultFigurePaperType', 'a4');
-            set(groot, 'defaultFigurePosition', [2 2 20 12]);
+            set(groot, 'defaultFigurePosition',  [2 2 20 12]);
+            set(groot, 'defaultFigurePaperPositionMode', 'auto');
+            set(groot, 'defaultFigurePaperOrientation',  'landscape');
+
+        case 'rand'
+            %% Set a random colour theme.
+            set(groot, 'defaultAxesColor',     [rand, rand, rand]);
+            set(groot, 'defaultAxesYColor',    [rand, rand, rand]);
+            set(groot, 'defaultAxesXColor',    [rand, rand, rand]);
+            set(groot, 'defaultFigureColor',   [rand, rand, rand]);
+            set(groot, 'defaultTextColor',     [rand, rand, rand]);
+            set(groot, 'defaultAxesGridColor', [rand, rand, rand]);
 
         %TODO: define more themes...
         otherwise
