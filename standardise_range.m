e450aa530d9a84864c6e1d2307029a0da24fb9f7
Stuart Knock <stuart.knock@gmail.com>
2018-12-21 22:06:58 +1000

    Reviews standardise_range(). Adds some basic checking of args.

--- standardise_range.m
+++ standardise_range.m
@@ -1,12 +1,11 @@
-%% Rescales data in a given range
+%% Rescales data to a given range, defaults to [0, 1].
 %
 % ARGUMENTS: 
-%    data   -- nD array in the range  [min value,  max value]
-%    [a, b] -- 2 element vector with the new range
+%    data -- nD array in the range  [min value,  max value]
+%    new_range -- 2 element vector [min, max] with the new range.
 %
 % OUTPUT:
-%    data   -- Standardised data in the range [a, b]
-%
+%    data -- Rescaled data in the range [min, max].
 %
 % AUTHOR:
 %     Paula Sanz-Leon (2018-12-21).
@@ -17,17 +16,26 @@
 %}
 % NOTES: See https://scikit-learn.org/stable/modules/preprocessing.html
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function data = standardise_range(data, new_range)
+    if nargin < 1 || isempty(data)
+        error(['PSL:' mfilename ':NoData'], ...
+               'You must specify data to standardise.');
+    end
+    if nargin > 1 && ~isempty(new_range) && (numel(new_range) ~= 2)
+        error(['PSL:' mfilename ':BadRange'], ...
+               'new_range should be a two element vector.');
+    end
 
-if nargin < 2
-    new_range = [0 1];
-end
+    min_val = min(data(:));
+    max_val = max(data(:));
 
-min_val = min(data(:));
-max_val = max(data(:));
+    % Rescale to range [0,1]
+    data = (data - min_val) / (max_val - min_val);
 
-data = (data - min_val) / (max_val - min_val);
-data = data * (new_range(2) - new_range(1)) + new_range(1);
-
+    if nargin > 1 && ~isempty(new_range)
+        % Rescale to range new_range
+        data = data * (new_range(2) - new_range(1)) + new_range(1);
+    end
 
 end % function standardise_range()
