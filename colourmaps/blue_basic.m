3b88edb86488cf3fdc59cb716df66a88d929ef6e
Stuart Knock <stuart.knock@gmail.com>
2018-12-25 17:16:47 +1000

    Adds some 'basic' colourmaps.

---
+++ colourmaps/blue_basic.m
@@ -0,0 +1,55 @@
+%% Return a colormap array [m,3] -- simple blue, dark to light.
+%
+% ARGUMENTS:
+%    m -- number of colours in colormap.
+%    order -- ['fwd'|'rev'] ordering of returned colormap array.
+%
+% OUTPUT:
+%    c -- [m,3] colormap array.
+%
+% REQUIRES:
+%
+% AUTHOR:
+%     Stuart A. Knock (2018-12-24).
+%
+% USAGE:
+%{
+    
+%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [c] = blue_basic(m, order)
+    %% If number of colours (m) not specified, try setting from current colormap.
+    if nargin < 1 || isempty(m)
+       f = get(groot, 'CurrentFigure');
+       if isempty(f)
+          m = size(get(groot, 'DefaultFigureColormap'), 1);
+       else
+          m = size(f.Colormap, 1);
+       end
+    end
+
+    if nargin < 2  || isempty(order)
+        order = 'fwd';
+    end
+
+    %% Linear blue gradient.
+    bcm = [  0,   0,   1; ...
+             0,   0, 128; ...
+             0,   0, 255  ...
+                           ] ./ 255.0;
+
+    %% Number of colours in basis colormap.
+    nc = size(bcm, 1);
+
+    %% Colour step size to produce m colours for output.
+    cstep = (nc - 1) / (m - 1);
+
+    %% Linear interpolation of basis colormap.
+    c = interp1(1:nc, bcm, 1:cstep:nc);
+
+    if strcmp(order, 'rev') % reverse colormap
+        c = c(end:-1:1, :);
+    end
+
+end % function blue_basic()
